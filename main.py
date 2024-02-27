75d9d70488cbae1c62818e49b367f45e62e38a0a
kblc9l <97379286+kblc9l@users.noreply.github.com>
2024-02-27 17:12:28 +0400

    Добавляем капитана

--- main.py
+++ main.py
@@ -1,87 +1,59 @@
-import os
+from flask import Flask
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+from data import db_session
+from data.users import User
 
-from flask import Flask, render_template, request
+SqlAlchemyBase = orm.declarative_base()
+
+__factory = None
 
 app = Flask(__name__)
-href = '../static/img/img_2.png'
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template('base.html', title=title)
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    user.surname = "Scott"
+    user.name = 'Ridley'
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = 'module_1'
+    user.email = 'scott_chief@mars.org'
+    db_sess = db_session.create_session()
+    db_sess.add(user)
 
+    user = User()
+    user.surname = "Scott2"
+    user.name = 'Ridley2'
+    user.age = 211
+    user.position = "captain12123"
+    user.speciality = "research engineerasdad"
+    user.address = 'module_1adsads'
+    user.email = 'scott_chief@mars.orgasdadasdaSD'
+    db_sess = db_session.create_session()
+    db_sess.add(user)
 
-@app.route("/training/<prof>")
-def train(prof):
-    return render_template("prif.html", prof=prof)
+    user = User()
+    user.surname = "Scott2QWEEQEW"
+    user.name = 'Ridley2QWEQWEQE'
+    user.age = 2113
+    user.position = "captain12123QWEQWEE"
+    user.speciality = "research engineerasdadQDWDQWE"
+    user.address = 'module_1adsadQDWDQWDQDSs'
+    user.email = 'scott_chief@mars.orgasdadasdaSDQDSDQD'
+    db_sess = db_session.create_session()
+    db_sess.add(user)
 
+    db_sess.commit()
 
-@app.route('/results/<nickname>/<level>/<rating>')
-def res(nickname, level, rating):
-    return render_template('result.html', nickname=nickname, level=str(level), rating=str(rating))
-
-
-@app.route('/load_photo', methods=['POST', 'GET'])
-def load_photo():
-    global href
-    if request.method == 'GET':
-        return f'''<!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>Title</title>
-                        <link rel="stylesheet" type='text/css' href="../static/css/style.css">
-                    </head>
-                    <body>
-                    <h1>Загрузите фотографии</h1>
-                    <h2>для участия в миссии</h2>
-
-
-                    <form method="post" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label for="photo">Приложите фотографию</label>
-                            <input type="file" class="form-control-file" id="photo" name="file">
-                            <img src='{href}' alt="">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Отправить</button>
-                    </form>
-
-
-                    </body>
-                    </html>
-'''
-    elif request.method == 'POST':
-
-        f = request.files['file']
-        href = '../static/img/2.png'
-        if f:
-            f.save('static/img/2.png')
-            return f'''<!DOCTYPE html>
-                                <html lang="en">
-                                <head>
-                                    <meta charset="UTF-8">
-                                    <title>Title</title>
-                                    <link rel="stylesheet" type='text/css' href="../static/css/style.css">
-                                </head>
-                                <body>
-                                <h1>Загрузите фотографии</h1>
-                                <h2>для участия в миссии</h2>
-    
-    
-                                <form method="post" enctype="multipart/form-data">
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                        <img src='{href}' alt="">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                </form>
-    
-    
-                                </body>
-                                </html>'''
+    user = db_sess.query(User).first()
+    print(user.name)
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080)
+    main()
