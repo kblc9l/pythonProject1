e5e5ca49f8000ed1cda30cdcd9f37f97bf7b2e6d
kblc9l <97379286+kblc9l@users.noreply.github.com>
2024-02-27 17:25:51 +0400

    Добавляем капитана

--- data/users.py
+++ data/users.py
@@ -1,7 +1,5 @@
 import datetime
 import sqlalchemy
-from sqlalchemy import orm
-
 from .db_session import SqlAlchemyBase
 
 
@@ -10,15 +8,13 @@ class User(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    surname = sqlalchemy.Column(sqlalchemy.String)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    age = sqlalchemy.Column(sqlalchemy.Integer)
-    position = sqlalchemy.Column(sqlalchemy.String)
-    speciality = sqlalchemy.Column(sqlalchemy.String)
-    address = sqlalchemy.Column(sqlalchemy.String)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String)
-    modified_date = sqlalchemy.Column(sqlalchemy.DateTime)
-
-    # jobs = orm.relationship("Jobs", back_populates='user')
-    # news = orm.relationship("News", back_populates='user')
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import datetime
+
 from flask import Flask
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 from data import db_session
+from data.jobs import Jobs
 from data.users import User
 
 SqlAlchemyBase = orm.declarative_base()
@@ -15,43 +18,20 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    user = User()
-    user.surname = "Scott"
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
-    db_sess = db_session.create_session()
-    db_sess.add(user)
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now
+    job.is_finished = False
 
-    user = User()
-    user.surname = "Scott2"
-    user.name = 'Ridley2'
-    user.age = 211
-    user.position = "captain12123"
-    user.speciality = "research engineerasdad"
-    user.address = 'module_1adsads'
-    user.email = 'scott_chief@mars.orgasdadasdaSD'
     db_sess = db_session.create_session()
-    db_sess.add(user)
-
-    user = User()
-    user.surname = "Scott2QWEEQEW"
-    user.name = 'Ridley2QWEQWEQE'
-    user.age = 2113
-    user.position = "captain12123QWEQWEE"
-    user.speciality = "research engineerasdadQDWDQWE"
-    user.address = 'module_1adsadQDWDQWDQDSs'
-    user.email = 'scott_chief@mars.orgasdadasdaSDQDSDQD'
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-
+    db_sess.add(job)
     db_sess.commit()
 
-    user = db_sess.query(User).first()
-    print(user.name)
+    job = db_sess.query(Jobs).first()
+    print(job.name)
     app.run()
 
 
