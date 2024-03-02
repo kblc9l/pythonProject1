0425565a1611763abcfde1bc71cc9de25e9fd15f
kblc9l <97379286+kblc9l@users.noreply.github.com>
2024-03-02 16:13:12 +0400

    ещё один запрос

--- main.py
+++ main.py
@@ -1,31 +1,108 @@
 import datetime
 
-from flask import Flask
+import sqlalchemy
+
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from sqlalchemy.orm import Session
-from data import db_session
-from data.jobs import Jobs
-from data.users import User
 
 SqlAlchemyBase = orm.declarative_base()
 
 __factory = None
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def global_init(db_file):
+    global __factory
+
+    if __factory:
+        return
+
+    if not db_file or not db_file.strip():
+        raise Exception("Необходимо указать файл базы данных.")
+
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    print(f"Подключение к базе данных по адресу {conn_str}")
+
+    engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+
+    #  from . import __all_models
+
+    SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session():
+    global __factory
+    return __factory()
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'<Colonist> {self.id} {self.surname} {self.name} {self.age} years'
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    job = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
+
+    def __repr__(self):
+        return f'{self.team_leader}'
+
+
+class Department(SqlAlchemyBase):
+    __tablename__ = 'departaments'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    chief = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    members = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    user = orm.relationship('User')
 
 
 def main():
     name_db = input()
-    db_session.global_init(name_db)
-    # db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-    people = db_sess.query(User).filter(User.address == 'module_1')
-    print(*people)
+    global_init(name_db)
+    # global_init("db/blogs.db")
+    db_sess = create_session()
+    departments = db_sess.query(Department).all()
+    for d in departments:
+        members_of_department = d.members.split(", ")
+        for idx in members_of_department:
+            sum_hours = 0
+            for job in db_sess.query(Jobs).all():
+                if str(idx) in job.collaborators:
+                    sum_hours += job.work_size
+            if sum_hours > 25:
+                user = db_sess.query(User).filter(User.id == int(idx)).first()
+                print(user.name, user.surname)
 
-    app.run()
-
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
